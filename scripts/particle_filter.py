adb4bf8f3ad72fbed258122aa1b8c53624578898
Adam Weider <adamweider98@gmail.com>
2021-02-01 05:35:51 -0600

    Particle initialization.

--- scripts/particle_filter.py
+++ scripts/particle_filter.py
@@ -1,12 +1,18 @@
 from sensor_msgs.msg import LaserScan
 
+import lib.particle as particle
 from lib.particle_filter_base import ParticleFilterBase
 
 
 class ParticleFilter(ParticleFilterBase):
     def initialize_particle_cloud(self) -> None:
-        # TODO
-        pass
+        while self.map is None:
+            pass
+
+        self.particle_cloud = particle.from_occupancy_grid(
+            self.map,
+            self.num_particles,
+        )
 
     def normalize_particles(self) -> None:
         # TODO
